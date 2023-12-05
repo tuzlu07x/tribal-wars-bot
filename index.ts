2b6df45130cc85f309f5945b5e2e6261bff7a9b6
fatihtuzlu <fatihtuzlu07@gmail.com>
2023-12-06 02:26:59 +0300

    fixed

--- index.ts
+++ index.ts
@@ -1,4 +1,10 @@
 import PuppeteerConnection from "./src/Puppeteer/Connection";
-
+const connection = new PuppeteerConnection(
+  "https://www.klanlar.org/",
+  "username",
+  "lordFatih",
+  "password",
+  "1234567890"
+);
 
 connection.connector();
