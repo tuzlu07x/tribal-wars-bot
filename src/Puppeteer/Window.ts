884ec7e1f37d36a5807ba30cdbfe6ef31c32e74b
fatihtuzlu <fatihtuzlu07@gmail.com>
2023-12-10 14:52:30 +0300

    session is worked.

--- src/Puppeteer/Window.ts
+++ src/Puppeteer/Window.ts
@@ -9,30 +9,26 @@ export default class Window {
   public async start(credentials: TWCredentials) {
 
     const page = await this.agent.newPage();
-    // try {
-    //   await this.agent.loadSession()
-    //   await page.goto(credentials.mainUrl, { waitUntil: "networkidle0" });
+    try {
+      await this.agent.loadSession()
+      await page.goto(credentials.mainUrl, { waitUntil: "networkidle0" });
 
-    // } catch (error) {
-    console.log('anana1')
-    await page.goto(credentials.mainUrl);
-    await this.auth(credentials.username, credentials.password);
-    await this.agent.saveSession();
-    // }
+    } catch (error) {
+      console.log('anana1')
+      await page.goto(credentials.mainUrl);
+      await this.auth(credentials.username, credentials.password);
+      await this.agent.saveSession();
+    }
   }
 
   private async auth(username: string, password: string): Promise<void> {
-    console.log(username, password)
+
     await this.waitForSelector(`input[name='username']`);
-    console.log(1)
     await this.type(`input[name='username']`, username);
-    console.log(2)
     await this.waitForSelector(`input[name='password']`);
-    console.log(3)
     await this.type(`input[name='password']`, password);
-    console.log(4)
     await this.clickLoginButton('btn-login');
-    console.log(5)
+    await this.waitForNavigation({ waitUntil: "networkidle0" })
   }
 
   private async clickLoginButton(clasName: string): Promise<void> {
@@ -57,6 +53,13 @@ export default class Window {
 
     const page = await this.agent.newPage();
     await page.click(`.${name}`);
+
+  }
+
+  private async waitForNavigation(name: object): Promise<void> {
+
+    const page = await this.agent.newPage();
+    await page.waitForNavigation(name);
 
   }
 }
